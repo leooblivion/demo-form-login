bdda88a3774f75d69e54d2e58277645e6c61b4e9
leooblivion <leooblivion24@gmail.com>
2020-02-20 04:37:16 +0700

    Fix Animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 function login(y) {
     document.getElementsByClassName('form-login')[0].style.left = '25%';
-    document.getElementsByClassName('form-login')[0].style.opacity = '0';
+    document.getElementsByClassName('form-login')[0].style.visibility = 'hidden';
     document.getElementsByClassName('form-register')[0].style.left = '50%';
-    document.getElementsByClassName('form-register')[0].style.opacity = '1';
+    document.getElementsByClassName('form-register')[0].style.visibility = 'visible';
 }
 
 function register() {
     document.getElementsByClassName('form-login')[0].style.left = '50%';
-    document.getElementsByClassName('form-login')[0].style.opacity = '1';
+    document.getElementsByClassName('form-login')[0].style.visibility = 'visible';
     document.getElementsByClassName('form-register')[0].style.left = '75%';
-    document.getElementsByClassName('form-register')[0].style.opacity = '0';
+    document.getElementsByClassName('form-register')[0].style.visibility = 'hidden';
 }
